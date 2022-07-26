4c8a4bacde717ff01bc26cf80f401d4343ddbefb
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-07-27 07:36:03 +0800

    minor refactor

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
+function addRedmineButton() {
+  let button = document.createElement("input");
+  button.id = "redmine_card_button";
+  button.type = "button";
+  button.value = "Open Redmine Card";
+
+  button.onclick = () => {
+    alert("Redmine Card");
+  };
+  document
+    .querySelector(
+      "#partial-discussion-header > div.d-flex.flex-items-center.flex-wrap.mt-0.gh-header-meta > div.flex-shrink-0.mb-2.flex-self-start.flex-md-self-center"
+    )
+    .appendChild(button);
+}
+
 (() => {
-  function addRedmineButton() {
-    let button = document.createElement("input");
-    button.id = "redmine_card_button";
-    button.type = "button";
-    button.value = "Open Redmine Card";
-
-    button.onclick = () => {
-      alert("Redmine Card");
-    };
-    document
-      .querySelector(
-        "#partial-discussion-header > div.d-flex.flex-items-center.flex-wrap.mt-0.gh-header-meta > div.flex-shrink-0.mb-2.flex-self-start.flex-md-self-center"
-      )
-      .appendChild(button);
-  }
-
   addRedmineButton();
 })();
