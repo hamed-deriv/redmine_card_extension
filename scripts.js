40e3da957f69370e80bf6e717e66c83f07eccb0a
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-07-27 09:59:43 +0800

    add on click functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
 function addRedmineButton() {
   let button = document.createElement("input");
+
   button.id = "redmine_card_button";
   button.type = "button";
   button.value = "Open Redmine Card";
 
-  button.onclick = () => {
-    alert("Redmine Card");
-  };
   document
     .querySelector(
       "#partial-discussion-header > div.d-flex.flex-items-center.flex-wrap.mt-0.gh-header-meta > div.flex-shrink-0.mb-2.flex-self-start.flex-md-self-center"
     )
     .appendChild(button);
+
+  button.onclick = () => {
+    let pullRequestTitle = document.querySelector(
+      "#partial-discussion-header > div.gh-header-show > div > h1 > span.js-issue-title.markdown-title"
+    );
+
+    let pullRequestNumber = pullRequestTitle.textContent
+      .split("/")[1]
+      .replace("#", "");
+
+    window.open("https://redmine.deriv.cloud/issues/" + pullRequestNumber);
+  };
 }
 
 (() => {
